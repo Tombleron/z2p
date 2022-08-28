77c671f0dcee69f75a1c3efff092fbbc3576d2ee
Tombleron <fokindacer@gmail.com>
2022-08-28 14:02:26 +0700

    Added secrecy crate to protect private data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use z2p::{configuration::get_configuration, logging::*, run};
 
@@ -10,7 +11,7 @@ async fn main() -> std::io::Result<()> {
     // Load app configuration (NOTE: stored in config.yaml)
     let config = get_configuration().expect("Error loading configuration.");
 
-    let connection = PgPool::connect(&config.database.connection_string())
+    let connection = PgPool::connect(&config.database.connection_string().expose_secret())
         .await
         .expect("Error connecting to db.");
 
--- src/routes/subscriber.rs
+++ src/routes/subscriber.rs
@@ -4,7 +4,6 @@ use actix_web::{web, HttpResponse, Responder};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -17,24 +16,31 @@ pub struct Subscriber {
 ///
 /// If data is invalid or there's some error with database
 /// this method will yield Internal Server Error
-pub async fn subscribe(
-    data: web::Form<Subscriber>,
-    connection: web::Data<PgPool>,
-) -> impl Responder {
-    let request_id = Uuid::new_v4();
+#[tracing::instrument(
+    name = "Adding new user",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name= %form.name
+    )
+)]
+pub async fn subscribe(form: web::Form<Subscriber>, pool: web::Data<PgPool>) -> impl Responder {
+    match insert_subscriber(&form, &pool).await {
+        Ok(_) => HttpResponse::Ok(),
+        Err(_) => HttpResponse::InternalServerError(),
+    }
+}
 
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
-        subscriber_email = %data.email,
-        subscriber_name= %data.name
-    );
-
-    let _request_span_guard = request_span.enter();
-
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Inserting new user to database",
+    skip(data, pool),
+)]
+async fn insert_subscriber(
+    data: &Subscriber,
+    pool: &PgPool,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -44,14 +50,11 @@ pub async fn subscribe(
         data.name,
         Utc::now()
     )
-    .execute(connection.get_ref())
-    .instrument(query_span)
+    .execute(pool)
     .await
-    {
-        Ok(_) => HttpResponse::Ok(),
-        Err(e) => {
-            tracing::error!("Failed to execute query: {:?}", e);
-            HttpResponse::InternalServerError()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
